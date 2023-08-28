98a5bbfcc88da749da8585c70e436becbf327c7a
4NDrew-42 <100322188+4NDrew-42@users.noreply.github.com>
2023-08-28 16:22:29 -0500

    CF:FS 1.1
    basic setup of pokedex-js website

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,7 @@
 alert('Hello world');
+
+let favoriteFood = 'sushi'; // I particularly like uni nigiri, and saba nigiri
+document.write(favoriteFood); 
+
+let simpleAddition = 1 + 1; 
+document.write(simpleAddition);
