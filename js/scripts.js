cdb683387e51dfd21536324038efe83a83953f2b
4NDrew-42 <100322188+4NDrew-42@users.noreply.github.com>
2023-08-28 22:52:47 -0500

    CF:FS 1.3
    CSS styling started
    
    Display pokemonList array to page with conditional if-else on height variable

--- js/scripts.js
+++ js/scripts.js
@@ -2,5 +2,15 @@ let pokemonList = [
     {name: 'Bulbasaur', height: 0.7, types: ['grass', 'poison']},
     {name: 'Charmander', height: 0.6, types: ['fire']},
     {name: 'Squirtle', height: 0.5, types: ['water']},
-] ;
+] 
 
+
+// Displaying the pokemonList in the page
+
+for (let i = 0; i < pokemonList.length; i++) {
+    if (pokemonList[i].height > 0.6) {
+        document.write('<p>' + pokemonList[i].name + ' (height: ' + pokemonList[i].height + ')' + ' - Wow, that\'s big!' + '</p>');
+    } else {
+        document.write('<p>' + pokemonList[i].name + ' (height: ' + pokemonList[i].height + ')' + '</p>');
+    }
+}
