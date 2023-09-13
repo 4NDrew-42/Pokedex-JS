2634900a9d27313e9a609efb49efa71dde468a44
4NDrew-42 <100322188+4NDrew-42@users.noreply.github.com>
2023-09-12 23:27:20 -0500

    CF:FS1.5 Pokedex, Add layout, search, verification

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,79 @@
-let pokemonList = [
-    {name: 'Bulbasaur', height: 0.7, types: ['grass', 'poison']},
-    {name: 'Charmander', height: 0.6, types: ['fire']},
-    {name: 'Squirtle', height: 0.5, types: ['water']},
-] 
+// IIFE
+let pokemonRepository = (function () {
+    let pokemonList = [ 
+      {name: 'Bulbasaur', height: 0.7, types: ['grass', 'poison']},
+      {name: 'Charmander', height: 0.6, types: ['fire']},
+      {name: 'Squirtle', height: 0.5, types: ['water']},
+    ];
+    
+    function add(pokemon) {
+        let validKeys = ['name', 'height', 'types'];
+        
+        if (
+          typeof pokemon === 'object' &&
+          Object.keys(pokemon).every(key => validKeys.includes(key)) &&
+          Object.keys(pokemon).length === validKeys.length &&
+          typeof pokemon.name === 'string' &&
+          typeof pokemon.height === 'number' &&
+          Array.isArray(pokemon.types) && 
+          pokemon.types.every(type => typeof type === 'string')
+        ) {
+          pokemonList.push(pokemon);
+        } else {
+          console.error('Invalid data type or invalid keys. Please provide a valid Pokemon object.');
+        }
+      }
+      
+    function getAll() {
+      return pokemonList;
+    }
+    
+    function findByName(name) {
+        if (typeof name !== 'string') {
+          console.error('Please provide a valid pokemon name.');
+          return [];
+        }
+        
+        return pokemonList.filter(pokemon => pokemon.name.toLowerCase() === name.toLowerCase());
+      }
+    
+      return {
+        add: add,
+        getAll: getAll,
+        findByName: findByName
+      };
+    })();
 
-
+    document.getElementById('findPokemonButton').addEventListener('click', function() {
+        const pokemonName = document.getElementById('pokemonName').value;
+        const results = pokemonRepository.findByName(pokemonName);
+    
+        const resultDiv = document.getElementById('pokemonResult');
+        resultDiv.innerHTML = '';  // Clear previous results
+    
+        if (results.length > 0) {
+            results.forEach(pokemon => {
+                const pokemonElement = document.createElement('div');
+                const nameElement = document.createElement('p');
+                const heightElement = document.createElement('p');
+                const typesElement = document.createElement('p');
+    
+                nameElement.textContent = `Name: ${pokemon.name}`;
+                heightElement.textContent = `Height: ${pokemon.height}`;
+                typesElement.textContent = `Types: ${pokemon.types.join(', ')}`;
+    
+                pokemonElement.appendChild(nameElement);
+                pokemonElement.appendChild(heightElement);
+                pokemonElement.appendChild(typesElement);
+                resultDiv.appendChild(pokemonElement);
+            });
+        } else {
+            resultDiv.textContent = 'No Pokémon found with that name.';
+        }
+    });
+    
 // Displaying the pokemonList in the page
 
-for (let i = 0; i < pokemonList.length; i++) {
-    if (pokemonList[i].height > 0.6) {
-        document.write('<p>' + pokemonList[i].name + ' (height: ' + pokemonList[i].height + ')' + ' - Wow, that\'s big!' + '</p>');
-    } else {
-        document.write('<p>' + pokemonList[i].name + ' (height: ' + pokemonList[i].height + ')' + '</p>');
-    }
-}
+pokemonRepository.getAll().forEach(function(pokemon) {
+    document.write('<p>' + pokemon.name + ' (height: ' + pokemon.height + ')' + '</p>');
+    });
